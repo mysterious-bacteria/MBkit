9ac7eb7081d6bd591f586e47b5098fa19afe0d9d
mysterious-bacteria <779208005@qq.com>
2025-04-08 19:25:28 +0800

    commit

--- log.py
+++ log.py
@@ -1,9 +1,57 @@
 import inspect
 import os
+import colorama
+import time
 
-def log(string):
+log_format = "{time} | {level} | {filename_only}: {line} - {function} | {string}"
+time_format = "{year}-{month}-{day}"
+
+colorama.init(True)
+
+
+class LogLevel:
+    DEBUG = "Debug"
+    TIP = "Tip"
+    INFO = "Info"
+    WARRING = "Warning"
+    ERROR = "Error"
+    CRASH = "Crash"
+    FATAL_ERROR = "Fatal error"
+
+
+def log(string, log_level=LogLevel.INFO):
+    now_time = time.localtime()
+    time_str = time_format.format(year=now_time.tm_year,
+                                  month=now_time.tm_mon,
+                                  day=now_time.tm_mday,
+                                  hour=now_time.tm_hour,
+                                  minute=now_time.tm_min,
+                                  sec=now_time.tm_sec)
+
     frame = inspect.currentframe()
-    filename = os.path.basename(frame.f_code.co_filename)
+    caller_frame = inspect.stack()[1]
+
+    filename_full = caller_frame.filename
+    filename_only = os.path.basename(filename_full)
+
     line = frame.f_back.f_lineno
-    print(f"{filename}:{line} - {string}")
-log("log")
+    func = caller_frame.function
+
+    message = log_format.format(filename_full=filename_full,
+                                filename_only=filename_only,
+                                line=line,
+                                string=string,
+                                function=func,
+                                level=log_level,
+                                time=time_str)
+
+    if log_level == LogLevel.FATAL_ERROR or log_level == LogLevel.CRASH:
+        print(colorama.Back.RED + message)
+    elif log_level == LogLevel.ERROR or log_level == LogLevel.WARRING:
+        print(colorama.Back.YELLOW + message)
+    elif log_level == LogLevel.INFO:
+        print(message)
+    elif log_level == LogLevel.TIP:
+        print(colorama.Fore.BLUE + message)
+    else:
+        print(message)
--- requests/download.py
+++ requests/download.py
@@ -1,21 +1,15 @@
-import os
 import requests
 # TODO tqdm
 
-def download_file(url, local_filename):
 
+def download_file(url, filepath):
     response = requests.get(url, stream=True)
-    
+
     if response.status_code == 200:
-        #TODO muilti-threaded download
-        with open(local_filename, 'wb') as f:
+        #TODO multi-threaded download
+        with open(filepath, 'wb') as f:
             for chunk in response.iter_content(chunk_size=4096):
                 f.write(chunk)
-        print(f"Downloaded {local_filename}")
+        return 0
     else:
-        print(f"Failed to download {url}: {response.status_code}")
-
-if __name__ == "__main__":
-    url = "https://blitsdl.blitsgames.com/jockstudio/JockStudio-Demo-Win.zip"
-    local_filename = "file.txt"
-    download_file(url, local_filename)
+        return 1
